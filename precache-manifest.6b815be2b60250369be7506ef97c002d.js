04ca73f2d207f090809345574910beb1832d3524
Serkan <serkangurer@yahoo.com>
2020-04-29 14:22:21 +0300

    Updates

---
+++ precache-manifest.6b815be2b60250369be7506ef97c002d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81255105c32156dc792291089e912f2a",
+    "url": "/kittycats/index.html"
+  },
+  {
+    "revision": "793d3fbec03e507dd51d",
+    "url": "/kittycats/static/css/main.f88d06aa.chunk.css"
+  },
+  {
+    "revision": "390803279c9480efe3ea",
+    "url": "/kittycats/static/js/2.048273be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittycats/static/js/2.048273be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793d3fbec03e507dd51d",
+    "url": "/kittycats/static/js/main.33c17e00.chunk.js"
+  },
+  {
+    "revision": "6a047fa541284ad217ba",
+    "url": "/kittycats/static/js/runtime-main.74bf0b09.js"
+  }
+]);
